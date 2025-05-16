00c53fed601db6daf94515e2800df7c5b5fd8dcd
Win Metcalf <winmetcalf@Wins-MacBook-Pro-2.local>
2025-05-15 21:46:28 -0400

    second push

--- the_ai.py
+++ the_ai.py
@@ -15,6 +15,7 @@ class TheAi:
         for i in self.connected_nodes:
             power_tick += i.bonus
         self.power += power_tick
+        self.board.update_board(self.power)
         print(self.power)
 
 
